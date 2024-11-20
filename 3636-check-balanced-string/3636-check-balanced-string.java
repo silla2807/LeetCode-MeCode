8d21a012c6ed23561099ee2f339d35b0a888e3d3
Silla K <79395251+silla2807@users.noreply.github.com>
2024-11-20 11:25:15 +0530

    Time: 1 ms (99.36%), Space: 42.1 MB (71.56%) - LeetHub

---
+++ 3636-check-balanced-string/3636-check-balanced-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int i=0;i<num.length();i++){
+            if(i%2 == 0) evenSum+=  num.charAt(i)-'0';
+            else oddSum += num.charAt(i)-'0';
+        }
+        return oddSum == evenSum;
+    }
+}
