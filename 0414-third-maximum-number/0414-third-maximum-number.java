7227df8e6fc5ef964b20083e37c2c131da31d6f8
Silla K <79395251+silla2807@users.noreply.github.com>
2024-11-21 21:51:28 +0530

    Time: 1 ms (97.56%), Space: 43.2 MB (45.56%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -5,16 +5,16 @@ class Solution {
         long thirdMax = Long.MIN_VALUE;   
 
         for (int num : nums) {
-            if (num == max || num == secondMax || num == thirdMax) continue;
+            // if (num == max || num == secondMax || num == thirdMax) continue;
 
             if (num > max) {
                 thirdMax = secondMax;
                 secondMax = max;
                 max = num;
-            } else if (num > secondMax) {
+            } else if (num > secondMax && num != max ) {
                 thirdMax = secondMax;
                 secondMax = num;
-            } else if (num > thirdMax) {
+            } else if (num > thirdMax && num != secondMax && num != max) {
                 thirdMax = num;
             }
         }
