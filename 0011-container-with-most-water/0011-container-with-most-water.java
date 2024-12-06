40666185b602bdbeadbddedf410c1fb45c991aa9
Silla K <79395251+silla2807@users.noreply.github.com>
2024-12-06 22:23:11 +0530

    Time: 5 ms (73.74%), Space: 58.3 MB (14.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,11 +3,8 @@ class Solution {
         int left=0, right = height.length-1;
         int volume = Integer.MIN_VALUE;
         while(left<right){
-            System.out.print(left+" "+right+" ");
             int currentQuant = (right-left)*(Math.min(height[left],height[right]));
-
             volume=Math.max(volume,currentQuant);
-            System.out.println(currentQuant+" "+volume);
             if(height[left]<height[right]){
                 left++;
             }else{
