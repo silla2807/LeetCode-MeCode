dcc4f70dc6d426591fe6c875b1408f613dd2db2b
Silla K <79395251+silla2807@users.noreply.github.com>
2024-12-16 20:34:57 +0530

    Time: 1 ms (100%), Space: 44.9 MB (50.55%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
         int min = 0;
-
-       //consider k=1, 
-       //find min
-       //multiply the element in the array at the index of min with the multiplier
-
-       for(int i=0;i<k;i++){
-        min=0;
-        for(int j=0;j<nums.length;j++){
-            min = nums[j] < nums[min] ? j : min; 
+        //find min
+        //multiply the element in the array at the index of min with the multiplier
+        
+        for(int i=0;i<k;i++){
+            min=0;
+            for(int j=0;j<nums.length;j++){
+                min = nums[j] < nums[min] ? j : min;
+                }
+            nums[min]*=multiplier;
+            }
+        return nums;
         }
-        nums[min]*=multiplier;
-    }
-    return nums;
-}
 }
