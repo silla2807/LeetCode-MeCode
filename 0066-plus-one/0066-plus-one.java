cbce20af470955f901a84da3692b6e7bb1b210a3
Silla K <79395251+silla2807@users.noreply.github.com>
2025-01-02 15:58:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (81.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] plusOne(int[] digits) {
         
         for(int i=digits.length-1;i >= 0;i--){
-            if(digits[i]+1 != 10){
+            if(digits[i]+1 <= 9){
                 digits[i] += 1;
                 return digits;
             }
